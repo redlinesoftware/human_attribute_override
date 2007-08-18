da48a5698660050e4caed0737f670c012d71701a
Andrew Kaspick <andrew@redlinesoftware.com>
2007-08-18 07:13:59 +0000

    fix to allow symbol or string attribute access

--- lib/human_attribute_override.rb
+++ lib/human_attribute_override.rb
@@ -7,6 +7,7 @@ module ActiveRecord
       # converted to 'Num employees' normally using <tt>human_attribute_name</tt>.  More descriptive text can be set. Example:
       #   attr_human_name 'num_employees' => 'Number of employees'
       def attr_human_name(attributes) # :nodoc:
+        attributes.stringify_keys!
         write_inheritable_hash("attr_human_name", attributes || {})
       end
 
@@ -18,7 +19,7 @@ module ActiveRecord
       # Transforms attribute key names into a more humane format, such as "First name" instead of "first_name". Example:
       #   Person.human_attribute_name("first_name") # => "First name"
       def human_attribute_name(attribute_key_name) #:nodoc:
-        (read_inheritable_attribute("attr_human_name") || {})[attribute_key_name] || attribute_key_name.humanize
+        (read_inheritable_attribute("attr_human_name") || {})[attribute_key_name.to_s] || attribute_key_name.to_s.humanize
       end
     end
   end
