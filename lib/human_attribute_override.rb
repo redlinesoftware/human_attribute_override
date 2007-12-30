3128c995b059fb4df213f1339c2ca74c23ba55d1
Andrew Kaspick <andrew@redlinesoftware.com>
2007-12-30 11:20:07 +0000

    the human_attribute code still exists in rails, even though it was at one time going to be removed... so this code is not required

--- lib/human_attribute_override.rb
+++ lib/human_attribute_override.rb
@@ -38,25 +38,4 @@ module ActiveRecord
       end
     end
   end
-
-  class Errors
-    # Returns all the full error messages in an array.
-    def full_messages
-      full_messages = []
-
-      @errors.each_key do |attr|
-        @errors[attr].each do |msg|
-          next if msg.nil?
-
-          if attr == "base"
-            full_messages << msg
-          else
-            full_messages << @base.class.human_attribute_name(attr) + " " + msg
-          end
-        end
-      end
-      
-      full_messages
-    end
-  end
 end
